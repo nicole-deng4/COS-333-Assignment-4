3c620a684e56fbf3baf87725755a21c06d431c84
nicole-deng4 <nd6753@princeton.edu>
2025-11-11 13:08:48 -0500

    Add files via upload

---
+++ testregdetailsgiven.py
@@ -0,0 +1,87 @@
+#-----------------------------------------------------------------------
+# testregdetails.py
+# Author: Bob Dondero
+#-----------------------------------------------------------------------
+
+import sys
+import time
+import argparse
+import playwright.sync_api
+
+#-----------------------------------------------------------------------
+
+MAX_LINE_LENGTH = 72
+UNDERLINE = '-' * MAX_LINE_LENGTH
+
+#-----------------------------------------------------------------------
+
+def get_args():
+
+    parser = argparse.ArgumentParser(
+        description='Test the ability of the reg application to '
+            + 'handle "primary" (class overviews) queries')
+
+    parser.add_argument(
+        'serverURL', metavar='serverURL', type=str,
+        help='the URL of the reg application')
+
+    parser.add_argument(
+        'browser', metavar='browser', type=str,
+        choices=['firefox', 'chrome'],
+        help='the browser (firefox or chrome) that you want to use')
+
+    args = parser.parse_args()
+
+    return (args.serverURL, args.browser)
+
+#-----------------------------------------------------------------------
+
+def print_flush(message):
+
+    print(message)
+    sys.stdout.flush()
+
+#-----------------------------------------------------------------------
+
+def run_test(server_url, browser_process, classid):
+
+    print_flush(UNDERLINE)
+
+    try:
+        page = browser_process.new_page()
+        page.goto(server_url)
+
+        link = page.get_by_text(classid).first
+        link.click()
+
+        page.wait_for_selector('#classDetailsTable')
+        class_details_table = page.locator('#classDetailsTable')
+        print_flush(class_details_table.inner_text())
+
+        page.wait_for_selector('#courseDetailsTable')
+        course_details_table = page.locator('#courseDetailsTable')
+        print_flush(course_details_table.inner_text())
+
+    except Exception as ex:
+        print(str(ex), file=sys.stderr)
+
+
+#-----------------------------------------------------------------------
+
+def main():
+
+    server_url, browser = get_args()
+
+    with playwright.sync_api.sync_playwright() as pw:
+
+        if browser == 'chrome':
+            browser_process = pw.chromium.launch()
+        else:
+            browser_process = pw.firefox.launch()
+
+        run_test(server_url, browser_process, '8321')
+
+        # Add more tests here.
+
+if __name__ == '__main__':
+    main()
